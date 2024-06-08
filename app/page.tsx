6363c04414ebef9bdf186c836e31577ed5229cf8
unknown <dosiaczkowa@gmail.com>
2024-06-08 12:47:22 +0200

    Dodanie nowych plików

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <main>
+            <h1 className="text-3xl text-red-400">Daria Trela</h1>
+            <p>Nr albumu: 119189</p>
+        </main>
+    );
+}
